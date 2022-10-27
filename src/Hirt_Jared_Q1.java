3bd61165dc95fdbf9c68f0db574dd96692ebcadf
Jared Hirt <jaredhirt@hotmail.com>
2022-10-27 10:35:20 -0700

    commit

--- src/Hirt_Jared_Q1.java
+++ src/Hirt_Jared_Q1.java
@@ -1,7 +1,26 @@
 public class Hirt_Jared_Q1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    public static
+        double principal = 10000;
+        for(int i=0; i<=5; i++)
+        {
+            System.out.printf("Money in account after %d years $%.2f %n", i, principal);
+            principal = interestYear(principal);
+        }
 
-}
+    }
+    public static double interestYear(double principal){
+        for(int i = 1; i <= 6; i++)
+            principal = principal * 0.9d * 1.1d;
+        return principal;
+        }
+    }
+
+    /**
+     OUTPUT
+     Money in account after 0 years $10000.00
+     Money in account after 1 years $9414.80
+     Money in account after 2 years $8863.85
+     Money in account after 3 years $8345.14
+     Money in account after 4 years $7856.78
+     Money in account after 5 years $7397.00
+     */
--- src/Hirt_Jared_Q2.java
+++ src/Hirt_Jared_Q2.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class Hirt_Jared_Q2 {
+    public static void main(String[] args)
+    {
+        Scanner kbd = new Scanner(System.in);
+        System.out.println("Please enter the first number for the necklace");
+        int firstNumber = kbd.nextInt();
+        System.out.println("Please enter the second number for the necklace");
+        int secondNumber = kbd.nextInt();
+        for(int)
+    }
+    public static int necklace(int a, int b)
+    {
+        return (a + b)%10;
+    }
 }
